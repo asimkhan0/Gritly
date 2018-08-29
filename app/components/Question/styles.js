e3cb33edf34ace8a25f0ef18e56d7aef25e4c200
Asim Khan <khanasim034@gmail.com>
2018-08-30 00:40:08 +0500

    Question Component UI Fix

--- app/components/Question/styles.js
+++ app/components/Question/styles.js
@@ -2,7 +2,6 @@ import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
     container: {
-        backgroundColor: '#fff',
         marginTop: 8,
         //ios shadow
         shadowOpacity: 0.3,
@@ -16,6 +15,7 @@ export default StyleSheet.create({
         marginBottom:2
     },
     questionText: {
+        backgroundColor: '#fff',
         color: '#253851',
         fontSize: 14,
         paddingHorizontal: 10,
@@ -23,7 +23,19 @@ export default StyleSheet.create({
 
     },
     radioGroup: {
-        flexDirection: 'row'
+        backgroundColor: '#fff',
+        flexDirection: 'row',
+
+        //ios shadow
+        shadowOpacity: 0.3,
+        shadowRadius: 3,
+        shadowOffset: {
+            height: 0,
+            width: 0
+        },
+        //android shadow
+        elevation: 1,
+        marginTop:1
     },
     radioOptionText: {
         color: '#253851',
